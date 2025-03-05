8284f74517a1bd3662e9fa75c83ca4d4d05259d0
FurqanGit00 <furqanbusiness0@gmail.com>
2025-03-06 04:50:11 +0500

    add BMI

---
+++ BMI_calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double weight , height , BMI;
+	
+	cout << "Enter your weight in kg:";
+	cin >> weight;
+	
+	cout << "Enter your height in meters";
+	cin >> height ;
+	
+	BMI = weight / (height * height);
+	
+	cout << "Your BMI is:" << BMI ;
+	
+	if(BMI < 18.5)
+	{
+		cout << "\nUnderweight";
+	}
+	else if(BMI < 25.0)
+	{
+		cout << "\nNormal";
+	}
+	else if(BMI < 30.0)
+	{
+		cout << "\nOverweight";
+	}
+	else
+	{
+		cout << "\nObese";
+	}
+
+}
