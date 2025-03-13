48b842de12e4ef3e46328e09a56d196a5dd0df24
mFurqan0 <furqanbusiness0@gmail.com>
2025-03-13 07:10:42 +0500

    add new

---
+++ vowelFinder.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+int main() {
+	char letter , lowerLetter;
+	
+	cout << "Enter a letter:";
+	cin >> letter;
+	
+	if(!isalpha(letter)) {
+		cout << "Error! The letter entered was not an aplhabet.";
+		return 0;
+	}
+	
+	
+	lowerLetter = tolower(letter);
+			
+			
+	if(lowerLetter=='a'||lowerLetter=='e'||lowerLetter=='i'||lowerLetter=='o'||lowerLetter=='u') {
+		cout << "The Entered Letter" << ' ' << letter << ' ' << "is a vowel" ;	
+	}
+	else
+	{
+		cout << "The Entered Letter" << ' ' << letter << ' ' << "is a consonant" ;
+	}
+
+	return 0;
+}
+
