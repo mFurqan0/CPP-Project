6b8763ea3dd59d5618908c7afc2b790058b1cd07
mFurqan0 <furqanbusiness0@gmail.com>
2025-03-26 23:35:14 +0500

    C++ program that takes a positive integer num and prints the sum of the first natural numbers

---
+++ sumOfNaturalNumber.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	
+	cout << "Enter value of num:";
+	cin>>num;
+
+	for(int i=1;i<=num;i++)
+	{
+		int sum=0;
+		
+		for(int j=1;j<=i;j++) {
+		
+		
+		cout <<j;
+		sum += j;
+		
+		if(j<i)
+			cout << "+";
+		
+
+ 	}
+ 	cout << "="<<sum;
+ 	cout << "\n";
+
+	}	
+
+	return 0;
+
+}
