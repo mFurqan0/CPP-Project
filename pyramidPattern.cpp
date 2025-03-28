8281fd6ec11471622583dda12e96903c852abb32
mFurqan0 <furqanbusiness0@gmail.com>
2025-03-29 04:51:30 +0500

    changes in pyramid pattern

--- pyramidPattern.cpp
+++ pyramidPattern.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 int main() {
-	
     int num;
     cout << "Enter number: ";
     cin >> num;
     
-    for(int i=1;i<=num;i++){
-    	for(int j=num - i;j>=1;j--)    cout <<"*";	
-    	for(int k=1;k<=i;k++)          cout << k;
-    	for(int l=i-1;l>=1;l--)		   cout << l;
-		cout << "\n";
-	}
-	return 0;
+    for(int row = 1; row <= num; row++) {
+        for(int j = num - row; j >= 1; j--) {
+            cout << "*";
+        }
+        for(int ascend = 1; ascend <= row; ascend++) { 
+            cout << ascend;
+        }
+        for(int descend = row - 1; descend >= 1; descend--) {
+            cout << descend;
+        }
+        cout << "\n";
+    }
+    return 0;
 }
