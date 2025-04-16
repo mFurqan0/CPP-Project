b8459198545cc135327ea9db96a798de4005c19c
mFurqan0 <furqanbusiness0@gmail.com>
2025-04-16 17:12:09 +0500

    removes odd digits from a number

---
+++ RemoveOddFromNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    string even_num;
+    
+    cout << "Enter number greater than zero:" ;
+    cin >> n;
+    
+    while(n>0) {
+        int last_digit = n%10;
+        n /=10;
+        if(last_digit%2==0) {
+            even_num += to_string(last_digit) ;
+        }
+        
+    }
+    if(even_num.empty()) {
+        cout << "0"; }
+    else {
+        cout << "result is: " << even_num; }    
+    
+}
